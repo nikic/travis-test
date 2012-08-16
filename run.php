c8f9fa16d3d6cd1f32f3c1167f18970a51fcdb80
Nikita Popov <nikic@php.net>
2012-08-16 19:30:28 +0200

    Show collations

--- run.php
+++ run.php
@@ -1,3 +1,6 @@
 <?php
 
-echo "Run!";
+$db = new PDO('mysql:host=localhost;dbname=information_schema', 'root');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+var_dump($db->query('SHOW COLLATIONS')->fetchAll());
