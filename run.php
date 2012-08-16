5f2ae555ce76046b1be09db649b386c93acefc10
Nikita Popov <nikic@php.net>
2012-08-16 19:37:52 +0200

    Add files needed for mysqli tests

--- run.php
+++ run.php
@@ -3,4 +3,6 @@
 $db = new PDO('mysql:host=localhost;dbname=information_schema', 'root');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-var_dump($db->query('SHOW COLLATION')->fetchAll());
+//var_dump($db->query('SHOW COLLATION')->fetchAll());
+
+require_once 'mysqli_table.php';
