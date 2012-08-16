b58d5704a3f3bd44a89bed63b63f5ab37f0b20f0
Nikita Popov <nikic@php.net>
2012-08-16 19:31:42 +0200

    Fix typo

--- run.php
+++ run.php
@@ -3,4 +3,4 @@
 $db = new PDO('mysql:host=localhost;dbname=information_schema', 'root');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-var_dump($db->query('SHOW COLLATIONS')->fetchAll());
+var_dump($db->query('SHOW COLLATION')->fetchAll());
